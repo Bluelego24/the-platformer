6ebbb5c386f77946f7c9a7842a282d411d9bb501
Bluelego24 <79273557+Bluelego24@users.noreply.github.com>
2021-03-22 10:13:17 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+namespace SpriteKind {
+    export const Wall = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.overlapsWith(otherSprite)) {
+        mySprite.setVelocity(0, -120)
+    }
+})
+let mySprite: Sprite = null
+let otherSprite: Sprite = null
+otherSprite = sprites.create(assets.image`myImage0`, SpriteKind.Wall)
+mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.ay += 300
+mySprite.setStayInScreen(true)
