83e4329cc820cc3bbb8f44838e34c100ceba1cad
Bluelego24 <79273557+Bluelego24@users.noreply.github.com>
2021-04-01 10:07:42 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,29 @@
 namespace SpriteKind {
-    export const Wall = SpriteKind.create()
+    export const wall = SpriteKind.create()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (mySprite.overlapsWith(otherSprite)) {
-        mySprite.setVelocity(0, -120)
+function Start () {
+    mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+    scene.cameraFollowSprite(mySprite)
+    controller.moveSprite(mySprite, 100, 0)
+    mySprite.ay = 480
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 14))
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile5`, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        mySprite.vy = -150
     }
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile2`, function (sprite, location) {
+    mySprite.destroy()
+    pause(100)
+    Start()
+})
 let mySprite: Sprite = null
-let otherSprite: Sprite = null
-otherSprite = sprites.create(assets.image`myImage0`, SpriteKind.Wall)
+scene.setBackgroundColor(8)
+scene.setBackgroundImage(assets.image`myImage1`)
+tiles.setTilemap(tilemap`level4`)
 mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 14))
+scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite, 100, 0)
-mySprite.ay += 300
-mySprite.setStayInScreen(true)
+mySprite.ay = 480
